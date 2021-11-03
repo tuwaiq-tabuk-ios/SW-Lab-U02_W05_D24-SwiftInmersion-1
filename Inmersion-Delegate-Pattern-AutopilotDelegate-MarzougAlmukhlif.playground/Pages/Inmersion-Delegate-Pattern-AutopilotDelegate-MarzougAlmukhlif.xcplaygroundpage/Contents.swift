2949dd94d557fc4442880d7a284c971f7f0710f2
Marzouq Almukhlif <mhster_nice@Marzouqs-MacBook-Pro.local>
2021-11-03 16:21:04 +0300

    Finish Inmersion-Delegate-Pattern-AutopilotDelegate playground

---
+++ Inmersion-Delegate-Pattern-AutopilotDelegate-MarzougAlmukhlif.playground/Pages/Inmersion-Delegate-Pattern-AutopilotDelegate-MarzougAlmukhlif.xcplaygroundpage/Contents.swift
@@ -0,0 +1,63 @@
+import Foundation
+
+protocol AutopilotDelegate {
+  func activateAutoPilot(origin: String, destination: String)
+}
+
+
+class DriverlessSellingPointCarHandler {
+  var delegate: AutopilotDelegate?
+  
+  func attenRequests() {
+    print("Attending requests")
+  }
+  
+  func rentDriverlessCab(origin: String,
+                         destination: String) {
+    delegate?.activateAutoPilot(origin: origin, destination: destination)
+  }
+  
+}
+
+
+class Car: AutopilotDelegate {
+  
+  init(handler: DriverlessSellingPointCarHandler) {
+    handler.delegate = self
+  }
+  
+  func activateAutoPilot(origin: String, destination: String) {
+    print("Car: Driving from \(origin) to \(destination)")
+  }
+  
+  func regulateInteriorTemperature() {
+    print("Regulating interior temperature ")
+  }
+  
+}
+
+
+
+class MiniVan: AutopilotDelegate {
+  
+  init(handler: DriverlessSellingPointCarHandler) {
+    handler.delegate = self
+  }
+  
+  func activateAutoPilot(origin: String, destination: String) {
+    print("MiniVan: Driving from \(origin) to \(destination)")
+  }
+  
+  func regulateInteriorTemperature() {
+    print("Regulating interior temperature ")
+  }
+  
+}
+
+
+let cabHandler = DriverlessSellingPointCarHandler()
+let car = Car(handler: cabHandler)
+//let miniVan = MiniVan(handler: cabHandler)
+
+cabHandler.rentDriverlessCab(origin: "Tabuk", destination: "Duba")
+
