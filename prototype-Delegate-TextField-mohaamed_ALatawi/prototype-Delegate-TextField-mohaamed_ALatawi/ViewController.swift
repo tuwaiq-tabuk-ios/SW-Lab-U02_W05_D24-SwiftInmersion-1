317b815884d617b48c5ee91b2090236cc060f5be
mohammed <mhmdalsbwty@gmail.com>
2021-11-03 16:47:24 +0300

    prototype-Delegate-TextField

---
+++ prototype-Delegate-TextField-mohaamed_ALatawi/prototype-Delegate-TextField-mohaamed_ALatawi/ViewController.swift
@@ -0,0 +1,51 @@
+
+  
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+  
+  @IBOutlet weak var demoTextfield: UITextField!
+  
+  var counter = 5
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    demoTextfield.delegate = self
+
+  }
+  func textFieldShouldBeginEditing(
+    _ textField: UITextField)
+  -> Bool {
+    print(#function)
+
+    return true
+
+  }
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    print(#function)
+
+  }
+  func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+    print(#function)
+    if counter <= 5{
+      return false
+    }
+        else{
+          return true
+        }
+
+    }
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+      textField.resignFirstResponder();
+      return true;
+  }
+  func textFieldShouldClear(_ textField: UITextField) -> Bool {
+    print(#function)
+    return true
+  }
+  }
+
+
+
